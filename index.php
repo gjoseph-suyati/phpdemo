14d283a3809eb28cc3c145be34dcba851cd1ffe4
Gigy Joseph <gjoseph@suyati.com>
2021-06-24 14:51:01 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>K12 Professional Development</title>
+  <style>
+    body {
+      color: #ffffff;
+      background-color: #0188cc;
+      font-family: Arial, sans-serif;
+      font-size: 14px;
+    }
+    
+    h1 {
+      font-size: 500%;
+      font-weight: normal;
+      margin-bottom: 0;
+    }
+    
+    h2 {
+      font-size: 200%;
+      font-weight: normal;
+      margin-bottom: 0;
+    }
+  </style>
+</head>
+ <body>
+  <?php echo'<h1>Hello World</h1>';?>
+ </body>
+</html>
